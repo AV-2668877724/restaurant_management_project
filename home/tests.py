63f7104472d351f35b8db26fdff5b6b2c06beda4
AV-2668877724 <varshneyanurag888@gmail.com>
2025-12-15 20:58:42 +0530

    63811

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.db import models
 
-# Create your tests here.
+class OrderStatus(models.Model):
+    name = models.Charfield(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+
+    order_number = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return self.order_number
