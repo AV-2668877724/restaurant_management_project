cb3866c592f83f466095391dab3fbe9bf10b4bca
AV-2668877724 <varshneyanurag888@gmail.com>
2025-12-16 10:33:07 +0530

    63813

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import CouponValidationView
 
 urlpatterns = [
     
+    path('coupons/validation/', CouponValidationView.as_view(),name='coupon-validation'),
 ]
